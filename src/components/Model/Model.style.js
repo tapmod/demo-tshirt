2dcb47613a01ab34a7012a33e63352ddec43832b
vitaliy <whosesomebody@gmail.com>
2019-11-08 19:32:35 +0200

    Updated styles pt5

--- src/components/Model/Model.style.js
+++ src/components/Model/Model.style.js
@@ -8,7 +8,7 @@ export const Wrapper = styled.div`
   background-color: #f5f5f5;
   iframe {
     border: none;
-    height: calc(100% - 50px);
+    height: calc(100% - 85px);
     width: 100%;
     opacity: ${({ isLoading }) => (isLoading ? 0 : 1)};
   }
